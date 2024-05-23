9686088e51a2453e165aceb9ec051e7cadd9c386
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2024-05-23 20:38:44 +0200

    fixed startup script

--- gs/launch.sh
+++ gs/launch.sh
@@ -1,6 +1,6 @@
 # Function to check if wlan1 is up
 does_wlan1_missing() {
-  iw dev wlan1 info &> /dev/null;   
+  ! iw dev wlan1 info &> /dev/null;   
 }
 
 interval=1
@@ -9,7 +9,7 @@ timeout=10
 counter=0
 
 # Loop until wlan1 is up or timeout is reached
-while ! does_wlan1_missing; do
+while does_wlan1_missing; do
     if [ $counter -ge $timeout ]; then
         echo "Timeout reached. wlan1 did not appear."
         exit 1
