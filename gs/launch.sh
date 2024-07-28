e942032350be59a31c79c4048abf8dcfae695e41
RomanLut <11955117+RomanLut@users.noreply.github.com>
2024-07-28 22:44:52 +0100

    adjusted launch script

--- gs/launch.sh
+++ gs/launch.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+# Function to check if a desktop environment is running
+is_desktop_running() {
+    # Check for common desktop environment processes
+    if pgrep -x "gnome-shell" > /dev/null || pgrep -x "plasmashell" > /dev/null || pgrep -x "xfce4-session" > /dev/null || pgrep -x "mate-session" > /dev/null || pgrep -x "cinnamon" > /dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 cd /home/pi/
 cd esp32-cam-fpv
 cd gs
 sudo airmon-ng check kill
 
-#sudo -E LD_LIBRARY_PATH=/usr/local/lib DISPLAY=:0 ./gs
-sudo -E LD_LIBRARY_PATH=/usr/local/lib SDL_VIDEODRIVER=kmsdrm ./gs
+if is_desktop_running; then
+    sudo -E LD_LIBRARY_PATH=/usr/local/lib SDL_VIDEODRIVER=kmsdrm ./gs
+else
+    sudo -E LD_LIBRARY_PATH=/usr/local/lib DISPLAY=:0 ./gs
+fi
+
 
 #reconnect wlan0 to access point
 sudo wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
