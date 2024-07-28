24dbb8b3b9cbf7f28c44198462f94715e4935cf1
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2024-07-28 02:41:52 +0200

    adjusted launch script

--- gs/launch.sh
+++ gs/launch.sh
@@ -5,7 +5,8 @@ cd esp32-cam-fpv
 cd gs
 sudo airmon-ng check kill
 
-sudo -E LD_LIBRARY_PATH=/usr/local/lib DISPLAY=:0 ./gs
+#sudo -E LD_LIBRARY_PATH=/usr/local/lib DISPLAY=:0 ./gs
+sudo -E LD_LIBRARY_PATH=/usr/local/lib ./gs
 
 #reconnect wlan0 to access point
 sudo wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
